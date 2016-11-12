db742521696f7259f414546768354b9090052264
pablo8421 <pablo_jsd-8@hotmail.com>
2016-11-11 19:04:56 -0600

    Agregados tests.py

--- sitmoduloregistro/quickstart/tests.py
+++ sitmoduloregistro/quickstart/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from quickstart.models import Acta
+from datetime import datetime
 
-# Create your tests here.
+class ActaTestCase(TestCase):
+    def setUp(self):
+        datos_acta = {
+        	'id_acta':1,
+        	'id_expediente':1,
+        	'fecha':datetime.now(),
+        	'asunto':'asunto',
+        	'firma':'firma'
+        }
+        self.documento_creado = self.client.post('/expediente_create/',datos_acta,format='json')
+        
+
+    def test_crear_acta(self):
+        self.assertEqual(self.documento_creado.Status, 'OK')

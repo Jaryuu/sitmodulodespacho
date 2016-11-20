3111e91150c509c5e52340277a5db15739ab4f60
cesaregm7 <cesaregm93@gmail.com>
2016-11-19 20:35:47 -0600

    test5

--- sitmoduloregistro/quickstart/tests.py
+++ sitmoduloregistro/quickstart/tests.py
@@ -13,7 +13,7 @@ class ActaTestCase(APITestCase):
 			"last_name": "User",
 			"email": "user@test.com",
 		}
-		datos_acta = {
+		datos_expediente = {
 			"solicitante": "Makoto Hidaka",
 			"tipo_solicitud": "2",
 			"asunto": "cambio",
@@ -22,24 +22,27 @@ class ActaTestCase(APITestCase):
 			"fecha_modificacion": "2016-11-11"
 		}
 		datos_acta = {
-			"solicitante": "Makoto Hidaka",
-			"tipo_solicitud": "2",
-			"asunto": "cambio",
-			"documentos": "serie de documentos",
-			"fecha_creacion": "2016-11-11",
-			"fecha_modificacion": "2016-11-11"
-		}
+			"id_expediente": 1,
+			"id_fecha": "2016-11-11",
+			"id_asunto": "Asunto de prueba",
+			"id_firma": "Firma de prueba"
+        }
 	persona = AuthUser(username='admin',password='admin123',is_superuser=1,is_staff=1,is_active=1)
 	persona.save()
 		
 	client = APIClient()
 	client.login(username='admin',password='admin123')
-	self.expediente_creado = self.client.post('/users/expediente_new',datos_acta,content_type='application/json',auth=('admin','admin123'))
+	self.expediente_creado = self.client.post('/users/expediente_new',datos_expediente,content_type='application/json',auth=('admin','admin123'))
 		
 	self.usuario_creado = self.client.post('/users/new',usuario,content_type='application/json',auth=('admin','admin123'))
+	
+	self.registro_creado = self.client.post('/users/acta_new',usuario,content_type='application/json',auth=('admin','admin123'))
 
     def test_crear_acta(self):
         self.assertEqual(self.expediente_creado.status_code, status.HTTP_200_OK)
 
 	def test_crear_usuario(self):
 		self.assertEqual(self.usuario_creado.status_code, status.HTTP_200_OK)
+		
+	def test_crear_registro_acta(self):
+		self.assertEqual(self.registro_creado.status_code, status.HTTP_200_OK)
